2919c2f77579952360f5d140933f9c167525107f
Alex <alex.belisleturcot@gmail.com>
2017-03-18 11:07:04 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd $1
-pwd
-ls -lrt
-export GIT_SHORT_HASH=`git rev-parse --short HEAD`
-
 curl -n -X PATCH https://api.heroku.com/apps/${HEROKU_APPNAME}/config-vars \
   -d '{
   "GIT_SHORT_HASH": "'"${GIT_SHORT_HASH}"'",
@@ -13,6 +8,3 @@ curl -n -X PATCH https://api.heroku.com/apps/${HEROKU_APPNAME}/config-vars \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $HEROKU_API_KEY" \
   -H "Accept: application/vnd.heroku+json; version=3"
-  
-git remote add heroku-remote https://$HEROKU_USERNAME:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APPNAME.git
-git push -f heroku-remote master
